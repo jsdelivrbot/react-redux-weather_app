2972f91102a7b55bea05f03aba682e83e8d96640
andrewdc92 <andrew.cordivari@gmail.com>
2017-01-11 10:26:40 -0800

    adds axios, get request to api goes through

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,18 @@
-const API_KEY = '6c52016ee32832629360d445dadbc2a5
-';
+import axios from 'axios';
+
+const API_KEY = 'b5fd75657e1283066e3d789170b07e49';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// root_url uses es6 template string syntax at the end
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  //returns a promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   //takes the existing function, take th eupdated one (onInputChante), and
   //set it equal to current
@@ -19,6 +23,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     //go get some weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' })
   }
 
   render(){
@@ -36,3 +42,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
