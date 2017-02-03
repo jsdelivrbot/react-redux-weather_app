55fbfc06f2c1c3c306cb37358bbabf7a8db20928
andrewdc92 <andrew.cordivari@gmail.com>
2017-02-03 11:25:14 -0800

    added https to axios request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'b5fd75657e1283066e3d789170b07e49';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 // root_url uses es6 template string syntax at the end
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
